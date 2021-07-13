ded73774246fe9e259f865421a4574c1d86725bb
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-13 14:21:04 -0700

    added more projects with links and updated resume

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,33 +28,47 @@ largeCard.addEventListener("click", function(event) {
 var card2 = document.querySelector("#card2");
 card2.addEventListener("click", function(event) {
     event.preventDefault();
-    window.open('https://github.com/Pao1aG/Weather-Dashboard', '_blank');
+    window.open('https://github.com/Pao1aG/Tech-Blog', '_blank');
 });
 
 //CARD3
 var card3 = document.querySelector("#card3");
 card3.addEventListener("click", function(event) {
     event.preventDefault();
-    window.open('https://github.com/Pao1aG/Password-Generator', '_blank');
+    window.open('https://github.com/Pao1aG/Cities2See', '_blank');
 });
 
 //CARD4
 var card4 = document.querySelector("#card4");
 card4.addEventListener("click", function(event) {
     event.preventDefault();
-    window.open('https://github.com/Pao1aG/Workday-Scheduler', '_blank');
+    window.open('https://github.com/Pao1aG/Weather-Dashboard', '_blank');
 });
 
 //CARD5
 var card5 = document.querySelector("#card5");
 card5.addEventListener("click", function(event) {
     event.preventDefault();
-    window.open('https://github.com/Pao1aG/Code-Quiz', '_blank');
-})
+    window.open('https://github.com/Pao1aG/Password-Generator', '_blank');
+});
 
 //CARD6
 var card6 = document.querySelector("#card6");
 card6.addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open('https://github.com/Pao1aG/Workday-Scheduler', '_blank');
+});
+
+//CARD7
+var card7 = document.querySelector("#card7");
+card7.addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open('https://github.com/Pao1aG/Code-Quiz', '_blank');
+})
+
+//CARD8
+var card8 = document.querySelector("#card8");
+card8.addEventListener("click", function(event) {
     event.preventDefault();
     window.open('https://github.com/Pao1aG/Team-Profile-Generator', '_blank');
 })
